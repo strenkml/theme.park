086277f9b3d65150963b250b8c1fb01c9e5500af
Matt Strenk <strenkml@users.noreply.github.com>
2023-03-26 01:53:54 -0400

    Change ascii header

--- css/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
+++ css/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
@@ -1,17 +1,19 @@
 
  //Custom Text Header //
  // ACSII slant font
+ // Website: https://patorjk.com/software/taag/
 
 // Needs to be inside <section id="login" class="shadow">
 // You must escape any backward slashes. "\\"
 
 let custom_text_header = `
 <div class="custom-text-header"> <pre>
-    __  ___      __  __ _              ________   ____ 
-   /  |/  /___ _/ /_/ /( )_____   ____/__  /__ \\ / __ \\
-  / /|_/ / __ \`/ __/ __/// ___/  / ___/ / /__/ // / / /
- / /  / / /_/ / /_/ /_  (__  )  / /    / // __// /_/ / 
-/_/  /_/\\__,_/\\__/\\__/ /____/  /_/    /_//____/\\____/  
+    ________                       __       
+   / ____/ /_  _____  _________   / /__   __
+  / /_  / / / / / _ \/ ___/_  /  / __/ | / /
+ / __/ / / /_/ /  __/ /    / /__/ /_ | |/ / 
+/_/   /_/\__, /\___/_/    /___(_)__/ |___/  
+        /____/                              
 </pre> </div>
 `;
 document.getElementById("login").innerHTML += custom_text_header
